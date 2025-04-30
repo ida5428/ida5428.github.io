e59ddc2fdd56c101cda556960c978f4590d97d46
ida5428 <adiv5428@gmail.com>
2025-04-30 22:52:40 +1000

    added scroll 'guide' button

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ document.addEventListener('DOMContentLoaded', () => {
         setTheme(prefersDark);
     }
 });
+
+
+// Function runs on scrolling
+window.onscroll = function() {scrollIndicator()};
+
+// Function to show scrolling indicator
+// Code from W3Schools, added my own if statement to change style opacity
+function scrollIndicator() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height);
+    if (scrolled >= 1) {
+        document.querySelector(".scroll-indicator").style.opacity = 0;
+        document.querySelector(".scroll-button").style.opacity = 1;
+    } else {
+        document.querySelector(".scroll-indicator").style.opacity = 1;
+        document.querySelector(".scroll-button").style.opacity = 0;
+    }
+}
