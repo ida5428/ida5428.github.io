62600dd5cc560a69723b845964a0d485eff5337b
ida5428 <adiv5428@gmail.com>
2025-06-11 13:27:24 +1000

    Modified project cards and added a scroll down indicator

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function setTheme(isDark) {
     const sunIcon = document.getElementById('theme-icon-sun');
     const moonIcon = document.getElementById('theme-icon-moon');
     if (isDark) {
-        document.body.classList.add('dark-mode');
+        document.documentElement.classList.add('dark-mode');
         sunIcon.className = 'bx bx-sun bx-sm'; // outline sun in dark mode
         moonIcon.className = 'bx bxs-moon bx-sm'; // filled moon in dark mode
     } else {
-        document.body.classList.remove('dark-mode');
+        document.documentElement.classList.remove('dark-mode');
         sunIcon.className = 'bx bxs-sun bx-sm'; // filled sun in light mode
         moonIcon.className = 'bx bx-moon bx-sm'; // outline moon in light mode
     }
@@ -17,7 +17,7 @@ function setTheme(isDark) {
 
 // Function to toggle theme
 function themeToggle() {
-    const isDark = !document.body.classList.contains('dark-mode');
+    const isDark = !document.documentElement.classList.contains('dark-mode');
     setTheme(isDark);
 }
 
@@ -37,29 +37,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // Function runs on scrolling and on window resize
-window.onscroll = function() {scrollIndicator()};
-window.onresize = function() {scrollIndicator()};
+window.onscroll = function() {
+    scrollIndicators()
+};
+window.onresize = function() {
+    scrollIndicators()
+};
 
 // Also check on initial load
-document.addEventListener('DOMContentLoaded', scrollIndicator);
+document.addEventListener('DOMContentLoaded', scrollIndicators);
 
 // Function to show scrolling indicator
-function scrollIndicator() {
+function scrollIndicators() {
     const scrollButton = document.querySelector(".scroll-button");
-
-    // Check if page is scrollable
-    if (document.body.scrollHeight <= document.documentElement.clientHeight) {
-        scrollButton.style.display = "none";
-        return;
-    }
+    const downArrows = document.querySelector(".down-indicator");
 
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height);
 
-    scrollButton.style.display = "none";
+    if (scrolled <= 0.05) {
+        downArrows.style.visibility = "visible";
+        downArrows.classList.remove("hidden");
+    } else {
+        downArrows.classList.add("hidden");
+    }
+
+    // Check if page is scrollable
+    if (document.body.scrollHeight <= document.documentElement.clientHeight) {
+        scrollButton.style.visibility = "hidden";
+        return;
+    }
 
     if (scrolled >= 0.999) {
-        scrollButton.style.display = "flex";
+        scrollButton.style.visibility = "visible";
+        scrollButton.classList.remove("hidden");
+    } else {
+        scrollButton.classList.add("hidden");
     }
 }
